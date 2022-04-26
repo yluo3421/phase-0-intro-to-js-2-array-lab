bdc717361913dbad72b6c9ebac6dff0be30d0528
root <yluo3421@gmail.com>
2022-04-26 16:11:22 -0400

    commit tree 1dedf4638c500a0c5d35cca572699f0b45aa5dd6

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const catsAppendcopy = [...cats, name];
+    return catsAppendcopy;
+}
+
+function prependCat(name) {
+    const catsPrependcopy = [name, ...cats];
+    return catsPrependcopy;
+}
+
+function removeLastCat() {
+    const catsRemoveLastCatcopy = cats.slice(0, cats.length - 1);
+    return catsRemoveLastCatcopy;
+}
+
+function removeFirstCat() {
+    const catsRemoveFirstCatcopy = cats.slice(1);
+    return catsRemoveFirstCatcopy;
+}
